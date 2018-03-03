fbb359a0a3a7ca8497986cae42be2440664e183b
ditpowuh <ditpowuh@gmail.com>
2018-03-03 21:03:18 +1100

    Update console.js

--- console.js
+++ console.js
@@ -1,54 +1,54 @@
-CipherScript.message("Loading Console...")
+CipherScript.popup(CipherScript.message("Loading Console..."))
 CipherScript.wait(1000)
 if (CipherScript.setup == true) {
   CipherScript.post("Console + Setup Success!")
   CipherScript.console = true;
-  CipherScript.message("Console Enabled.")
+  CipherScript.popup(CipherScript.message("Console Loaded!"))
   CipherScript.wait(1000)
 
   while (CipherScript.console == true) {
     var consoletext = prompt("Console:")
 
     var emptyerror = consoletext.charAt(0);
-    if (consoletext == "" || emptyerror == " ") {
-      CipherScript.error(2)
+    if (CipherScript.booleancheck(consoletext == "" || emptyerror == " ") == true) {
+      CipherScript.popup(CipherScript.error(2))
     }
     else {
-      CipherScript.message(consoletext)
+      CipherScript.popup(CipherScript.message(consoletext))
     }
 
     if (consoletext == "CipherScript.console") {
-      CipherScript.message(CipherScript.console)
+      CipherScript.popup(CipherScript.message(CipherScript.console))
     }
     if (consoletext == "CipherScript.setup") {
-      CipherScript.message(CipherScript.setup)
+      CipherScript.popup(CipherScript.message(CipherScript.setup))
     }
     if (consoletext == "CipherScript.CipherScript") {
-      CipherScript.message("ERROR")
+      CipherScript.popup(CipherScript.message("ERROR"))
     }
     if (consoletext == "CipherScript.off") {
-      CipherScript.message("false")
+      CipherScript.popup(CipherScript.message(CipherScript.off))
     }
     if (consoletext == "CipherScript.on") {
-      CipherScript.message("true")
+      CipherScript.popup(CipherScript.message(CipherScript.on))
     }
     if (consoletext == "CipherScript.debug") {
-      CipherScript.message(CipherScript.debug)
+      CipherScript.popup(CipherScript.message(CipherScript.debug))
     }
     if (consoletext == "CipherScript.temporary") {
-      CipherScript.message(CipherScript.temporary)
+      CipherScript.popup(CipherScript.message(CipherScript.temporary))
     }
     if (consoletext == "CipherScript.fixmode") {
-      CipherScript.message(CipherScript.fixmode)
+      CipherScript.popup(CipherScript.message(CipherScript.fixmode))
     }
 
   }
 }
 else if (CipherScript.setup == false) {
   CipherScript.console = false;
-  CipherScript.message("Console is unable to open as setup was unsuccessful.")
-  CipherScript.error(1)
+  CipherScript.popup(CipherScript.message("Console is unable to open as setup was unsuccessful."))
+  CipherScript.popup(CipherScript.error(1))
 }
 else {
-  CipherScript.error(0)
+  CipherScript.popup(CipherScript.error(0))
 }
