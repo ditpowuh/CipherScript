e07568c0043ca3548d292f78e79f92bec838e0bd
ditpowuh <ditpowuh@gmail.com>
2018-03-13 13:23:27 +1100

    Update console.js

--- console.js
+++ console.js
@@ -1,47 +1,69 @@
 CipherScript.popup(CipherScript.message("Loading Console..."))
 CipherScript.wait(1000)
 if (CipherScript.setup == true) {
-  CipherScript.post("Console + Setup Success!")
-  CipherScript.console = true;
-  CipherScript.popup(CipherScript.message("Console Loaded!"))
-  CipherScript.wait(1000)
+  CipherScript.enableroles("password_a", "password_m")
+  if (CipherScript.safemode == false) {
+    CipherScript.post("Console + Setup Success!")
+    CipherScript.console = true;
+    CipherScript.popup(CipherScript.message("Console Loaded!"))
+    CipherScript.wait(1000)
 
-  while (CipherScript.console == true) {
-    var consoletext = prompt("Console:")
+    while (CipherScript.console == true) {
+      var consoletext = prompt("Console:")
 
-    var emptyerror = consoletext.charAt(0);
-    if (CipherScript.booleancheck(consoletext == "" || emptyerror == " ") == true) {
-      CipherScript.popup(CipherScript.error(2))
-    }
-    else {
-      CipherScript.popup(CipherScript.message(consoletext))
-    }
+      var emptyerror = consoletext.charAt(0);
+      if (CipherScript.booleancheck(consoletext == "" || emptyerror == " ") == true) {
+        CipherScript.popup(CipherScript.error(2))
+      }
+      else {
+        CipherScript.popup(CipherScript.message(consoletext))
+      }
 
-    if (consoletext == "CipherScript.console") {
-      CipherScript.popup(CipherScript.message(CipherScript.console))
+      if (consoletext == "CipherScript.CipherScript") {
+        CipherScript.popup(CipherScript.message("ERROR"))
+        CipherScript.popup(CipherScript.error(3))
+      }
+      if (consoletext == "CipherScript.safemode") {
+        CipherScript.popup(CipherScript.message("ERROR"))
+        CipherScript.popup(CipherScript.error(3))
+      }
+      if (consoletext == "CipherScript.console") {
+        CipherScript.popup(CipherScript.message(CipherScript.console))
+      }
+      if (consoletext == "CipherScript.setup") {
+        CipherScript.popup(CipherScript.message(CipherScript.setup))
+      }
+      if (consoletext == "CipherScript.off") {
+        CipherScript.popup(CipherScript.message(CipherScript.off))
+      }
+      if (consoletext == "CipherScript.on") {
+        CipherScript.popup(CipherScript.message(CipherScript.on))
+      }
+      if (consoletext == "CipherScript.debug") {
+        CipherScript.popup(CipherScript.message(CipherScript.debug))
+      }
+      if (consoletext == "CipherScript.temporary") {
+        CipherScript.popup(CipherScript.message(CipherScript.temporary))
+      }
+      if (consoletext == "CipherScript.fixmode") {
+        CipherScript.popup(CipherScript.message(CipherScript.fixmode))
+      }
+      if (consoletext == "CipherScript.testmode") {
+        CipherScript.popup(CipherScript.message(CipherScript.testmode))
+      }
     }
-    if (consoletext == "CipherScript.setup") {
-      CipherScript.popup(CipherScript.message(CipherScript.setup))
+  }
+  else if (CipherScript.safemode == true) {
+    CipherScript.warn("SAFE MODE ENABLED.")
+    CipherScript.popup(CipherScript.message("Note: If you are entering safe mode because of certain things, please reinstall CipherScript."))
+    CipherScript.adminrequired()
+    if (CipherScript.administrator == true) {
+      CipherScript.popup(CipherScript.message("DETECTED!: You are admin."))
     }
-    if (consoletext == "CipherScript.CipherScript") {
-      CipherScript.popup(CipherScript.message("ERROR"))
+    CipherScript.modrequired()
+    if (CipherScript.moderator == true) {
+      CipherScript.popup(CipherScript.message("DETECTED!: You are mod."))
     }
-    if (consoletext == "CipherScript.off") {
-      CipherScript.popup(CipherScript.message(CipherScript.off))
-    }
-    if (consoletext == "CipherScript.on") {
-      CipherScript.popup(CipherScript.message(CipherScript.on))
-    }
-    if (consoletext == "CipherScript.debug") {
-      CipherScript.popup(CipherScript.message(CipherScript.debug))
-    }
-    if (consoletext == "CipherScript.temporary") {
-      CipherScript.popup(CipherScript.message(CipherScript.temporary))
-    }
-    if (consoletext == "CipherScript.fixmode") {
-      CipherScript.popup(CipherScript.message(CipherScript.fixmode))
-    }
-
   }
 }
 else if (CipherScript.setup == false) {
