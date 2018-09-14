6352d55a9260da08bbc896b00e1d8b81abd614a0
ditpowuh <ditpowuh@gmail.com>
2018-09-14 13:20:59 +1000

    Update index.js

--- Node.JS/index.js
+++ Node.JS/index.js
@@ -8,15 +8,10 @@ module.exports = {
   htmlroles: false,
 
   message: function(text) {
-    if (csconsole === true) {
-      console.log(text);
-    }
+    console.log(text);
     return (text);
   },
   post: function(text) {
-    if (csconsole === false) {
-      console.log("Exception!: CipherScript.post can log on console without any interruptions.");
-    }
     console.log(text);
   },
   wait: function(milliseconds) {
@@ -28,16 +23,11 @@ module.exports = {
     }
   },
   warn: function(text) {
-    if (csconsole === true) {
-      console.log("Warned.");
-    }
+    console.log("Warned.");
     alert("WARNING!");
     var warntext = confirm(text);
   },
   error: function(errorcode, popup) {
-    if (csconsole === false) {
-      console.log("Exception!: CipherScript.error can log on console without any interruptions.");
-    }
     console.log("ERROR DETECTED.");
     if (errorcode === 0) {
       console.log("Error Code: 0;");
@@ -89,7 +79,7 @@ module.exports = {
     else {
       return "ERROR";
     }
-  };
+  },
   squareroot: function(number) {
     if (typeof number == "number") {
       return Math.sqrt(number);
@@ -107,12 +97,9 @@ module.exports = {
     }
   },
   beforeunload: function() {
-    if (csconsole === false) {
-      console.log("Exception!: CipherScript.beforeunload can log on console without any interruptions.");
-    }
     console.log("CipherScript.beforeunload: This is used for 'Are you sure you want to leave?' prompt.");
     return "ERROR";
-  };
+  },
   checkstate: function(input) {
     if (typeof input == "number") {
       return "number";
@@ -129,93 +116,6 @@ module.exports = {
     else {
       return "ERROR";
     }
-  };
-  enableroles: function(adminpassword, modpassword) {
-    if (htmlroles === false) {
-      htmlroles = true;
-      if (csconsole === true) {
-        console.log("CipherScript.enableroles: Sucess!");
-      }
-      administrator = adminpassword;
-      moderator = modpassword;
-      if (csconsole === true) {
-        console.log("Admin Password Set!: " + adminpassword);
-        console.log("Mod Password Set!: " + modpassword);
-      }
-    }
-    else if (htmlroles === true) {
-      if (csconsole === true) {
-        console.log("CipherScript.htmlroles: Already set!");
-      }
-    }
   },
-  adminrequired: function() {
-    if (htmlroles === true) {
-      alert("ADMINISTRATOR REQUIRED!")
-      var checkpassword = prompt("Password?:")
-      if (checkpassword == administrator) {
-        alert("Correct!");
-        administrator = true;
-      }
-      else {
-        alert("Incorrect!");
-        if (csconsole === true) {
-          console.log("CipherScript.adminrequire: Wrong password!");
-        }
-      }
-    }
-    else if (htmlroles == false) {
-      if (csconsole === true) {
-        console.log("CipherScript.htmlroles: Roles have not been set up!");
-      }
-    }
-  },
-  modrequired: function() {
-    if (htmlroles === true) {
-      alert("MODERATOR REQUIRED!");
-      var checkpassword = prompt("Password?:");
-      if (checkpassword == moderator) {
-        alert("Correct!")
-        moderator = true;
-      }
-      else {
-        alert("Incorrect!")
-        if (csconsole === true) {
-          console.log("CipherScript.modrequire: Wrong password!");
-        }
-      }
-    }
-    else if (htmlroles === false) {
-      if (csconsole === true) {
-        console.log("CipherScript.htmlroles: Roles have not been set up!");
-      }
-    }
-  },
-  resetroles: function(adminpassword, modpassword) {
-    if (htmlroles === true) {
-      administrator = adminpassword;
-      moderator = modpassword;
-      if (csconsole === true) {
-        console.log("CipherScript.htmlroles: Roles have been resetted!");
-      }
-    }
-    else if (htmlroles === false) {
-      if (csconsole === true) {
-        console.log("CipherScript.htmlroles: Roles have not been set up!");
-      }
-    }
-  },
-  createfile: function(title, text) {
-    var element = document.createElement("a");
-    element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
-    element.setAttribute("download", title);
-    element.style.display = "none";
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
-    if (csconsole === true) {
-      console.log("Created and Downloaded file: " + title);
-    }
-  }
 
-}
+};
