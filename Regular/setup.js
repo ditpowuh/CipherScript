cb1e4e5632d9c83eb552599001e589b1fe9d2d46
ditpowuh <ditpowuh@gmail.com>
2019-01-28 12:06:45 +1100

    Update setup.js

--- Regular/setup.js
+++ Regular/setup.js
@@ -241,5 +241,13 @@ if (CipherScript.on === true && CipherScript.off === false) {
       console.log("Created and Downloaded file: " + title);
     }
   };
+  CipherScript.var = function(input1, input2) {
+    if (!input1 && !input2) {
+      return "ERROR";
+    }
+    else {
+      eval(input1 + " = \"" + input2 + "\"");
+    }
+  };
 
 }
